47b66a4084ba2dae03bc694879828651118866b3
mhj412 <hyeongjoo14@gmail.com>
2020-09-11 22:05:52 +0900

    footer add

--- src/App.vue
+++ src/App.vue
@@ -10,18 +10,22 @@
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
       <v-toolbar-title>초원세탁소</v-toolbar-title>
       <v-spacer/>
-      <v-btn icon to="/">
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
       <v-btn to="/about">
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
+      <v-btn icon to="/">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      
     </v-app-bar>
     <v-content>
         <router-view/>
     </v-content>
     
-
+    <v-footer app color="primary" dark absolute>
+      <v-spacer></v-spacer>
+      <div>&copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
   </v-app>
 </template>
 
