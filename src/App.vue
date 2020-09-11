35aa539bf17dfc15915bd40defb6641d3b37e168
mhj412 <hyeongjoo14@gmail.com>
2020-09-11 21:20:52 +0900

    vuetify setup& v btn how

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,36 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>|
-      <router-link to="/xxx">xxx</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app 
+      color="primary"
+      dark
+    >
+    
+      
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>초원세탁소</v-toolbar-title>
+      <v-spacer/>
+      <v-btn icon to="/">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn to="/about">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+        <router-view/>
+    </v-content>
+    
+
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+export default {
+  name: 'App'
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+  
 }
-</style>
+</script>
